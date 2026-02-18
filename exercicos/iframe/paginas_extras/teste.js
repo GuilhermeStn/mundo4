1ae9cf10441e8120b56b6edbe9bc483244fb064c
Guilherme dos santos <guilhermesantosmendes67@gmail.com>
2026-02-17 21:06:04 -0300

    Praticando mais o js
    tentando criar mais interações

---
+++ exercicos/iframe/paginas_extras/teste.js
@@ -0,0 +1,20 @@
+function sair(){
+    s = window.document.getElementById("saida")
+    alert("teste")
+    var teste = document.createElement("h2")
+    var test_text = document.createTextNode("teste de texto")
+    teste.appendChild(test_text)
+    document.body.append(teste)
+    teste.style.textAlign =  "center";
+
+}
+
+function again(){
+    but = document.getElementById("testando")
+    but = document.createElement("input","type:text")
+    document.body.appendChild(but)
+    but = document.createElement("button")
+    document.body.appendChild(but)
+    but.onclick(alert("oi"))
+
+}
