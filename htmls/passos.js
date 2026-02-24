8f0a2ac799f2eea5499af83ed447e3fa15e80ab2
Guilherme dos santos <guilhermesantosmendes67@gmail.com>
2026-02-23 22:33:45 -0300

    consegui mostrar o resultado de um conversor
    também consegui criar um elemento que mostrasse na tela a resposta de seu calculo, usando o innerhtml !

--- htmls/passos.js
+++ htmls/passos.js
@@ -1,8 +1,10 @@
 /// pegar valor digitado 
 
 function pegar(){
-    window.alert("ai papai");
     var numero = document.querySelector("input");
-    window.alert(numero.value)
-    
+    window.alert(numero.value);
+    var valor_dolar = numero.value / 5.17 ;
+    window.alert("o valor em dolar é " + valor_dolar);
+    var resposta = document.querySelector("#resposta");
+    resposta.innerHTML = "<h2>Em dolar você terá :  </h2>"+valor_dolar.toFixed(2);
 }
